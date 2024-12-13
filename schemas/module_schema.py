8913385bdd685b1d94fbaf9074ba43c9492adb88
mutiara-n <mutiara.nafysah11@gmail.com>
2024-12-13 19:35:32 +0700

    feat: added module schema

---
+++ schemas/module_schema.py
@@ -0,0 +1,44 @@
+create_module_schema = {
+    "course_id": {
+        "type": "integer",
+        "required": True,
+        "min": 1
+    },
+    "title": {
+        "type": "string",
+        "required": True,
+        "minlength": 3,
+        "maxlength": 255
+    },
+    "content": {
+        "type": "string",
+        "required": True,
+        "minlength": 3
+    },
+    "module_file": {
+        "type": "string",
+        "required": False,
+        "minlength": 3,
+        "maxlength": 255
+    }
+}
+
+update_module_schema = {
+    "title": {
+        "type": "string",
+        "required": True,
+        "minlength": 3,
+        "maxlength": 100
+    },
+    "content": {
+        "type": "string",
+        "required": True,
+        "minlength": 3
+    },
+    "module_file": {
+        "type": "string",
+        "required": False,
+        "minlength": 3,
+        "maxlength": 255
+    }
+}
