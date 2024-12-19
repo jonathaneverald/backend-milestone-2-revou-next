1133ae6c21d9c8b7b34fc1202139164168778cef
jonathaneverald <jonathaneverald@gmail.com>
2024-12-20 00:06:23 +0700

    feat: changed module schemas & fixing some bugs on module apis

--- controllers/module_controller.py
+++ controllers/module_controller.py
@@ -2,13 +2,15 @@ from flask import Blueprint
 from flask_jwt_extended import jwt_required
 from connector.mysql_connectors import connect_db
 from sqlalchemy.orm import sessionmaker
-from models import ModuleModel, AssessmentModel, RoleModel
+from models import ModuleModel, AssessmentModel, RoleModel, CourseModel
 from utils.handle_response import ResponseHandler
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from enums.enum import UserRoleEnum
 from cerberus import Validator
 from schemas.module_schema import create_module_schema, update_module_schema
+from services.upload import UploadFiles
+from werkzeug.datastructures import FileStorage
 
 module_bp = Blueprint("module", __name__)
 
@@ -37,15 +39,16 @@ def get_module_assessments(module_id):
         s.close()
 
 
-@module_bp.route("/api/v1/modules", methods=["POST"])
+@module_bp.route("/api/v1/courses/<int:course_id>/modules", methods=["POST"])
 @jwt_required()
-def create_module():
+def create_module(course_id):
     Session = sessionmaker(bind=connect_db())
     s = Session()
     s.begin()
 
     try:
-        data = request.get_json()
+        data = request.form.to_dict()  # Change to form data to handle file upload
+        module_file = request.files.get("module_file")
         user_id = get_jwt_identity()
 
         validator = Validator(create_module_schema)
@@ -57,13 +60,30 @@ def create_module():
         instructor_role = (
             s.query(RoleModel).filter(RoleModel.user_id == user_id, RoleModel.role == UserRoleEnum.instructor).first()
         )
-
         if not instructor_role:
             return ResponseHandler.error("Unauthorized user", 403)
 
+        # Check if course is available and belongs to the instructor
+        course = (
+            s.query(CourseModel).filter(CourseModel.id == course_id, CourseModel.role_id == instructor_role.id).first()
+        )
+        if not course:
+            return ResponseHandler.error("Course not found or belongs to other instructor", 403)
+
+        # Handle profile picture upload if provided
+        module_file_url = None
+        if module_file and isinstance(module_file, FileStorage):
+            upload_files = UploadFiles()
+            result = upload_files.process_single_file(module_file)
+
+            if "error" in result:
+                return ResponseHandler.error(f"Module file upload failed: {result['error']}", 400)
+
+            module_file_url = result["file_url"]
+
         # Create new module
         new_module = ModuleModel(
-            course_id=data["course_id"], title=data["title"], content=data["content"], module_file=data["module_file"]
+            course_id=course_id, title=data["title"], content=data["content"], module_file=module_file_url
         )
         s.add(new_module)
         s.commit()
@@ -78,18 +98,34 @@ def create_module():
         s.close()
 
 
-@module_bp.route("/api/v1/modules", methods=["GET"])
+@module_bp.route("/api/v1/courses/<int:course_id>/modules", methods=["GET"])
 @jwt_required()
-def get_all_modules():
+def get_all_modules(course_id):
     Session = sessionmaker(bind=connect_db())
     s = Session()
     s.begin()
 
     try:
-        modules = s.query(ModuleModel).filter(ModuleModel.course_id).all()
+        user_id = get_jwt_identity()
+
+        # Check if user is instructor
+        instructor_role = (
+            s.query(RoleModel).filter(RoleModel.user_id == user_id, RoleModel.role == UserRoleEnum.instructor).first()
+        )
+        if not instructor_role:
+            return ResponseHandler.error("Unauthorized user", 403)
+
+        # Check if course is available and belongs to the instructor
+        course = (
+            s.query(CourseModel).filter(CourseModel.id == course_id, CourseModel.role_id == instructor_role.id).first()
+        )
+        if not course:
+            return ResponseHandler.error("Course not found or belongs to other instructor", 403)
+
+        modules = s.query(ModuleModel).filter(ModuleModel.course_id == course.id).all()
 
         return ResponseHandler.success(
-            {"Modules": [module.to_dictionaries() for module in modules]}, "Modules retrieved successfully"
+            {"modules": [module.to_dictionaries() for module in modules]}, "Modules retrieved successfully"
         )
 
     except Exception as e:
@@ -99,19 +135,24 @@ def get_all_modules():
         s.close()
 
 
-@module_bp.route("/api/v1/modules/<int:module_id>", methods=["GET"])
+@module_bp.route("/api/v1/courses/<int:course_id>/modules/<int:module_id>", methods=["GET"])
 @jwt_required()
-def get_module_by_id(module_id):
+def get_module_by_id(course_id, module_id):
     Session = sessionmaker(bind=connect_db())
     s = Session()
     s.begin()
 
     try:
-        module = s.get(ModuleModel, module_id)
-        if not module:
-            return ResponseHandler.error("module not found", 404)
+        # Check if course is available and belongs to the instructor
+        course = s.query(CourseModel).filter(CourseModel.id == course_id).first()
+        if not course:
+            return ResponseHandler.error("Course not found", 404)
 
-        return ResponseHandler.success(module.to_dictionaries(), "module retrieved successfully")
+        module = s.query(ModuleModel).filter(ModuleModel.id == module_id, ModuleModel.course_id == course.id).first()
+        if not module:
+            return ResponseHandler.error("Module not found", 404)
+
+        return ResponseHandler.success(module.to_dictionaries(), "Module retrieved successfully")
 
     except Exception as e:
         return ResponseHandler.error(str(e), 500)
@@ -120,26 +161,23 @@ def get_module_by_id(module_id):
         s.close()
 
 
-@module_bp.route("/api/v1/modules/<int:module_id>", methods=["PATCH"])
+@module_bp.route("/api/v1/courses/<int:course_id>/modules/<int:module_id>", methods=["PATCH"])
 @jwt_required()
-def update_module(module_id):
+def update_module(course_id, module_id):
     Session = sessionmaker(bind=connect_db())
     s = Session()
     s.begin()
 
     try:
-        data = request.get_json()
+        data = request.form.to_dict()
+        module_file = request.files.get("module_file")
         user_id = get_jwt_identity()
 
         module = s.get(ModuleModel, module_id)
 
         validator = Validator(update_module_schema)
-
         if not validator.validate(data):
             return ResponseHandler.error("Validation error", 400, validator.errors)
-
-        if not module:
-            return ResponseHandler.error("Module not found", 404)
 
         # Check if user is instructor
         instructor_role = (
@@ -149,6 +187,17 @@ def update_module(module_id):
         if not instructor_role:
             return ResponseHandler.error("Unauthorized user", 403)
 
+        # Check if course is available and belongs to the instructor
+        course = (
+            s.query(CourseModel).filter(CourseModel.id == course_id, CourseModel.role_id == instructor_role.id).first()
+        )
+        if not course:
+            return ResponseHandler.error("Course not found or belongs to other instructor", 404)
+
+        module = s.query(ModuleModel).filter(ModuleModel.id == module_id, ModuleModel.course_id == course.id).first()
+        if not module:
+            return ResponseHandler.error("Module not found", 404)
+
         # Update module
         if "title" in data:
             module.title = data["title"]
@@ -156,8 +205,15 @@ def update_module(module_id):
         if "content" in data:
             module.content = data["content"]
 
-        if "module_file" in data:
-            module.module_file = data["module_file"]
+        # Handle profile picture upload if provided
+        if module_file and isinstance(module_file, FileStorage):
+            upload_files = UploadFiles()
+            result = upload_files.process_single_file(module_file)
+
+            if "error" in result:
+                return ResponseHandler.error(f"Module file upload failed: {result['error']}", 400)
+
+            module.module_file = result["file_url"]
 
         s.commit()
 
@@ -171,18 +227,15 @@ def update_module(module_id):
         s.close()
 
 
-@module_bp.route("/api/v1/modules/<int:module_id>", methods=["DELETE"])
-def delete_module(module_id):
+@module_bp.route("/api/v1/courses/<int:course_id>/modules/<int:module_id>", methods=["DELETE"])
+@jwt_required()
+def delete_module(course_id, module_id):
     Session = sessionmaker(bind=connect_db())
     s = Session()
     s.begin()
 
     try:
         user_id = get_jwt_identity()
-        module = s.get(ModuleModel, module_id)
-        if not module:
-            return ResponseHandler.error("Module not found", 404)
-
         # Check if user is instructor
         instructor_role = (
             s.query(RoleModel).filter(RoleModel.user_id == user_id, RoleModel.role == UserRoleEnum.instructor).first()
@@ -190,6 +243,17 @@ def delete_module(module_id):
 
         if not instructor_role:
             return ResponseHandler.error("Unauthorized user", 403)
+
+        # Check if course is available and belongs to the instructor
+        course = (
+            s.query(CourseModel).filter(CourseModel.id == course_id, CourseModel.role_id == instructor_role.id).first()
+        )
+        if not course:
+            return ResponseHandler.error("Course not found or belongs to other instructor", 404)
+
+        module = s.query(ModuleModel).filter(ModuleModel.id == module_id, ModuleModel.course_id == course.id).first()
+        if not module:
+            return ResponseHandler.error("Module not found", 404)
 
         s.delete(module)
         s.commit()
--- schemas/module_schema.py
+++ schemas/module_schema.py
@@ -1,44 +1,16 @@
 create_module_schema = {
-    "course_id": {
-        "type": "integer",
-        "required": True,
-        "min": 1
-    },
-    "title": {
-        "type": "string",
-        "required": True,
-        "minlength": 3,
-        "maxlength": 255
-    },
-    "content": {
-        "type": "string",
-        "required": True,
-        "minlength": 3
-    },
-    "module_file": {
-        "type": "string",
-        "required": False,
-        "minlength": 3,
-        "maxlength": 255
-    }
+    # "course_id": {
+    #     "type": "integer",
+    #     "required": True,
+    #     "min": 1
+    # },
+    "title": {"type": "string", "required": True, "minlength": 3, "maxlength": 255},
+    "content": {"type": "string", "required": True, "minlength": 3},
+    "module_file": {"type": "string", "required": False, "minlength": 3, "maxlength": 255},
 }
 
 update_module_schema = {
-    "title": {
-        "type": "string",
-        "required": True,
-        "minlength": 3,
-        "maxlength": 100
-    },
-    "content": {
-        "type": "string",
-        "required": True,
-        "minlength": 3
-    },
-    "module_file": {
-        "type": "string",
-        "required": False,
-        "minlength": 3,
-        "maxlength": 255
-    }
+    "title": {"type": "string", "required": False, "minlength": 3, "maxlength": 100},
+    "content": {"type": "string", "required": False, "minlength": 3},
+    "module_file": {"type": "string", "required": False, "minlength": 3, "maxlength": 255},
 }
